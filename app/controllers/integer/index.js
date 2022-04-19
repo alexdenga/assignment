69e59d6e797647ec4a9446ca7423d0a9c3f5005a
Alex Denga <denga@Alexs-MacBook-Pro.local>
2022-04-19 10:17:48 +0300

    logic

--- app/controllers/integer/index.js
+++ app/controllers/integer/index.js
@@ -1,7 +1,28 @@
 
 exports.getInteger = async (req, res, next) => {
       try {
-         
+        if (req.body.num){
+         if (req.body.num % 5 == 0) {
+            res.status(200).json({
+               message: "L"
+            });
+         } else if(req.body.num % 7 == 0) {
+            res.status(200).json({
+                message: "R"
+             });
+         } else if(req.body.num % 5 == 0 && req.body.num % 7 == 0) {
+            res.status(200).json({
+                message: "LR"
+             });
+         } else {
+            res.status(200).json({
+                message: "Not a multiple of 5 or 7"
+             });
+         }} else {
+            res.status(200).json({
+                message: "Please enter a number"
+             });
+         }
       } catch (error) {
           return res.status(500).json({ message: "Internal server error." })
       }
